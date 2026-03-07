0d480dc5ff29fb6ff89ff84fb613335d7338ec60
dhil <senthilsays@gmail.com>
2026-03-07 14:40:51 -0800

    Initial commit: Android WebView app for Next.js

---
+++ app/src/main/java/com/mynextjsapp/webview/MainActivity.java
@@ -0,0 +1,41 @@
+package com.mynextjsapp.webview;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    // Change this URL to your Next.js app's URL
+    private static final String NEXTJS_URL = "http://10.0.2.2:3000";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(NEXTJS_URL);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
